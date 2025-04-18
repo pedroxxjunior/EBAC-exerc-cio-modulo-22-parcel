43966b63ccb9530bf11467474176548c3e134984
Pedro mendonça junior <pedrojunior2112@gmail.com>
2025-04-17 23:25:51 -0400

    finalizando projeto

---
+++ src/scripts/main.js
@@ -0,0 +1,29 @@
+AOS.init();
+
+const dataDoEvento = new Date("Dec 22, 2025 20:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const mesMs = 1000 * 60 * 60 * 24 * 30;
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+
+    const mesAteOEvento = Math.floor(distanciaAteOEvento / mesMs);
+    const diasAteOEvento = Math.floor((distanciaAteOEvento % mesMs) / diaEmMs - 4);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minutoEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
+
+    document.getElementById('contador').innerHTML = `${mesAteOEvento}M ${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = 'O Tempo expirou';
+    }
+}, 1000);
